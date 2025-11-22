e2db3d7f0c48572c20712bd69d598a91ec69ebe4
nahhhu <nahucordero06@gmail.com>
2025-11-22 21:13:29 +0000

    creacion de algoritmo de ordenamiento bubble sort utilizando la base dada por los profesores

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -1,9 +1,8 @@
-# Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
-
 items = []
 n = 0
 i = 0
 j = 0
+
 
 def init(vals):
     global items, n, i, j
@@ -12,12 +11,28 @@ def init(vals):
     i = 0
     j = 0
 
+
 def step():
-    # TODO:
-    # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
-    # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
-    # 3) Avanzar punteros (preparar el próximo paso).
-    # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    #
-    # Cuando no queden pasos, devolvé {"done": True}.
-    return {"done": True}
+    global items, n, i, j
+
+    if i >= n:
+        return {"done": True}
+
+    if j < n - 1 - i:
+        a = j
+        b = j + 1
+        swap = False
+
+        if items[a] > items[b]:
+            temp = items[a]
+            items[a] = items[b]
+            items[b] = temp
+            swap = True
+
+        j += 1
+        return {"a": a, "b": b, "swap": swap, "done": False}
+
+    else:
+        j = 0
+        i += 1
+        return {"a": -1, "b": -1, "swap": False, "done": False}
