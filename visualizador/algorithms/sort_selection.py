1ff2cb454d00b4bcd27836b359262e0fae2d7ec5
Santiago Velazquez <santiagonahuelvelazquez05@gmail.com>
2025-11-27 04:12:29 -0300

    Algoritmo de ordenamiento por selección terminado

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -17,12 +17,35 @@ def init(vals):
     fase = "buscar"
 
 def step():
-    # TODO:
-    # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
-    #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
-    #   Al terminar el barrido, pasar a fase "swap".
-    # - Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
-    #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
-    #
-    # Cuando i llegue al final, devolvé {"done": True}.
-    return {"done": True}
+
+    global items, n, i, j, min_idx, fase
+    
+    #Si i es menor mayor que n significa que el recorrido terminó, por lo tanto retorna "done:true"
+    if i >= n:  
+        return{"done":True}
+
+    if fase == "buscar":  
+        if j < n :
+            j_actual = j
+            #Compara dos valores de la lista items con indices distintos para saber cual es menor
+            if items[j] < items[min_idx]:   
+                #si items[j] es menor que items[min_idx], j pasa a ser el nuevo indice donde se encuentra el elemento de menor valor
+                min_idx = j  
+            j +=1
+            return{"a": min_idx, "b": j_actual, "swap": False, "done": False}
+        else:
+                fase="swap"
+    if fase == "swap":
+        i_original = i
+        min_idx_original = min_idx
+        swap_hecho = False
+        if min_idx != i:  #verifica que min_idx sea distinto de i, sino el elemento está en la posición correcta
+            temp = items[i]
+            items[i] = items[min_idx]
+            items[min_idx] = temp
+            swap_hecho = True   
+        i+=1    #i aumenta uno para comparar el proximo valor
+        min_idx = i #reinicia el valor de min_idx en el valor de la cabeza de la parte no ordenada
+        j = i+1 #pone el valor de j en el siguiente valor de i para compararlos entre si
+        fase = "buscar" #reiniciamos la fase en "buscar" para que se reinicie la busqueda
+        return{"a":i_original, "b":min_idx_original, "swap":swap_hecho, "done":False}
